583afbbfb653f08badb38e0bd94d07c0e806fff1
Zephyer <justinzhangyf@gmail.com>
2025-08-23 17:04:58 +0800

    建造者模式

--- builder-design/src/main/java/Main.java
+++ builder-design/src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        User user = User.builder()
+                .name("Tom")
+                .age(19)
+                .build();
+        System.out.println(user.getName() + " " + user.getAge());
     }
 }
--- builder-design/src/main/java/User.java
+++ builder-design/src/main/java/User.java
@@ -3,19 +3,66 @@ public class User {
     private String name;
     private int age;
 
-    public String getName() {
-        return name;
+    private User() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+
+    public static class Builder {
+        private String name;
+        private int age;
+
+        private Builder() {
+        }
+
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        private void check() {
+            if (name == null) {
+                throw new IllegalArgumentException("name is null");
+            }
+
+            if (age <= 0) {
+                throw new IllegalArgumentException("age <= 0");
+            }
+
+            if (age > 120) {
+                throw new IllegalArgumentException("age > 120");
+            }
+
+            if (age < 18 && name.startsWith("Tom")) {
+                throw new IllegalArgumentException("");
+            }
+        }
+
+        public User build() {
+            User user = new User();
+            user.name = this.name;
+            user.age = this.age;
+
+            check();
+            return user;
+        }
+
     }
 }
