9c03906a290acb8011343e987124ab647108f3a8
Zephyer <justinzhangyf@gmail.com>
2025-08-27 00:47:24 +0800

    check synchronizedSet if thread safety

--- decorator-pattern/src/main/java/Main.java
+++ decorator-pattern/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +16,12 @@ public class Main {
         strSet.remove("Alice");
 
         System.out.println(strSet);
+
+        Set<String> synchronizedSet = Collections.synchronizedSet(strSet);
+
+        // 下方代码块并不是线程安全的，因为isEmpty和add两个操作不是原子操作
+        if (synchronizedSet.isEmpty()) {
+            synchronizedSet.add("Tommy");
+        }
     }
 }
