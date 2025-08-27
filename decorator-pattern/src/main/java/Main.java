53aeff9a207d258b9054299be28a4fba84f52e9c
Zephyer <justinzhangyf@gmail.com>
2025-08-27 18:47:40 +0800

    使用普通的fileInputStream流读取20M的文件

--- decorator-pattern/src/main/java/Main.java
+++ decorator-pattern/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,6 +26,40 @@ public class Main {
         // 下方代码块并不是线程安全的，因为isEmpty和add两个操作不是原子操作
         if (synchronizedSet.isEmpty()) {
             synchronizedSet.add("Tommy");
+        }
+
+//        byte num = -128;
+//        byte[] bytes = new byte[256];
+//
+//        for (int i = 0; i < bytes.length; i++) {
+//            bytes[i] = num;
+//            num++;
+//        }
+//
+//        System.out.println(Arrays.toString(bytes));
+//
+//        for (int i = 0; i < bytes.length; i++) {
+//            int unsignedByte = bytes[i] & 0xFF;
+//            System.out.println("Signed: " + bytes[i] + " -> Unsigned: " + unsignedByte);
+//        }
+
+        readFile();
+    }
+
+    // 理解Java IO流的装饰器模式
+    public static void readFile() {
+        File file = new File("C:\\Users\\Sweety\\IdeaProjects\\DesignPattern\\decorator-pattern\\jvm.pdf");
+        long epochMilli = Instant.now().toEpochMilli();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            while (true) {
+                int read = fileInputStream.read();
+                if (read == -1) {
+                    break;
+                }
+            }
+            System.out.println("Read time: " + (Instant.now().toEpochMilli() - epochMilli) + "ms");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
